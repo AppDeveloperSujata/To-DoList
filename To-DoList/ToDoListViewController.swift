00fc2ec8586e252b649504cda28bdf306c86a020
Sujata <sujata.shyam.123@gmail.com>
2018-08-22 16:16:30 +0530

    Add new item functionality

--- To-DoList/ToDoListViewController.swift
+++ To-DoList/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController
 {
-    let itemArray = ["Find Mike", "Buy Grocery", "Destroy Demo"]
+    var itemArray = ["Find Mike", "Buy Grocery", "Destroy Demo"]
    
     override func viewDidLoad()
     {
@@ -33,7 +33,6 @@ class ToDoListViewController: UITableViewController
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
         {
            tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -45,5 +44,28 @@ class ToDoListViewController: UITableViewController
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: Add New Items
+    
+    @IBAction func btnAdd_Click(_ sender: UIBarButtonItem)
+    {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New To Do Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField{
+            (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+    
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
