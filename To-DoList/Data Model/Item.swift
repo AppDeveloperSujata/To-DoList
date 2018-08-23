d6ed89eb1b6b03850ff26977f65d68e8c1c451e0
Sujata <sujata.shyam.123@gmail.com>
2018-08-23 12:59:14 +0530

    Convert To MVC design pattern

---
+++ To-DoList/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  To-DoList
+//
+//  Created by Sujata on 23/08/18.
+//  Copyright © 2018 Sujata Shyam. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title: String = ""
+    var done: Bool = false
+}
